f83d4c19303ddc916426f52f2e2e78a5f3e54d91
Suraj Sharma <63995300+Surajsharma07@users.noreply.github.com>
2022-07-15 12:10:44 +0530

    Create __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -182,7 +182,7 @@ help_string = f'''
 
 def bot_help(update, context):
     button = ButtonMaker()
-    button.buildbutton("Other Commands", f"@hydra_torrent_bot")
+    button.buildbutton("Other Commands", f"https://telegra.ph/{help}")
     reply_markup = InlineKeyboardMarkup(button.build_menu(1))
     sendMarkup(help_string, context.bot, update.message, reply_markup)
 
