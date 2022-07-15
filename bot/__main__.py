6e4b459c3163eb61911887c735c12460dd3ce324
Suraj Sharma <63995300+Surajsharma07@users.noreply.github.com>
2022-07-15 12:35:25 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -66,7 +66,7 @@ def stats(update, context):
 def start(update, context):
     buttons = ButtonMaker()
     buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    reply_markup = InlineKeyboardMarkup(buttons.build_menu(0))
+    reply_markup = InlineKeyboardMarkup(buttons.build_menu(1))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
 This bot can mirror all your links to Google Drive!
