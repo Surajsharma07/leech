bfb5b38e25f62f1237883d60fa15c143310a9ff8
Suraj Sharma <63995300+Surajsharma07@users.noreply.github.com>
2022-07-15 12:33:37 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -66,15 +66,15 @@ def stats(update, context):
 def start(update, context):
     buttons = ButtonMaker()
     buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+PRRzqHd31XY3ZWZk")
+    reply_markup = InlineKeyboardMarkup(buttons.build_menu(0))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
 This bot can mirror all your links to Google Drive!
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
-        sendMarkup(start_string, context.bot, update.message)
+        sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
-        sendMarkup('Not Authorized user! Only for Hydras.', context.bot, update.message)
+        sendMarkup('Not Authorized user! Only for Hydras.', context.bot, update.message, reply_markup)
 
 def restart(update, context):
     restart_message = sendMessage("Restarting...", context.bot, update.message)
